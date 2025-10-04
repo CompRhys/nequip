80546821d04c1e758a42657d2b0b164ffb88928c
Rhys Goodall <rhys.goodall@outlook.com>
2025-10-03 20:40:06 -0400

    fea: extend test to also check torchscript compile

--- tests/integration/test_compile.py
+++ tests/integration/test_compile.py
@@ -7,23 +7,27 @@ from nequip.scripts.compile import main
 @pytest.mark.parametrize(
     "device", ["cpu"] + (["cuda"] if torch.cuda.is_available() else [])
 )
-def test_compile_aotinductor_ase_target(tmp_path_factory, device):
+@pytest.mark.parametrize(
+    "mode,extension", [("aotinductor", ".nequip.pt2"), ("torchscript", ".nequip.pth")]
+)
+def test_compile_modes(tmp_path_factory, device, mode, extension):
     tmp_path = tmp_path_factory.mktemp("nequip_compiled")
-    output_model_name = "mir-group__NequIP-OAM-L__0.1.nequip.pt2"
+    output_model_name = f"mir-group__NequIP-OAM-L__0.1{extension}"
     output_path = Path(tmp_path) / output_model_name
 
-    main(
-        args=[
-            "nequip.net:mir-group/NequIP-OAM-L:0.1",
-            str(output_path),
-            "--mode",
-            "aotinductor",
-            "--device",
-            device,
-            "--target",
-            "ase",
-        ]
-    )
+    args = [
+        "nequip.net:mir-group/NequIP-OAM-L:0.1",
+        str(output_path),
+        "--mode",
+        mode,
+        "--device",
+        device,
+    ]
+
+    if mode == "aotinductor":
+        args.extend(["--target", "ase"])
+
+    main(args=args)
 
     assert output_path.exists()
     assert output_path.is_file()
